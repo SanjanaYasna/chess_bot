97638101ee3816a1f8b0d3096e47daa5eda38089
SanjanaYasna <sanyasna517@gmail.com>
2025-09-26 10:13:27 -0400

    fen start

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def game_iter():
     #false for when game outcome 
     return True
 
+def print_board(board):
+    print("\nCurrent Board Position:")
+    print("----------------------")
+    print(board)
+    print("----------------------\n")
+    
 if __name__ == "__main__":
     #have user 
     print("Computer Player? (w=white/b=black):")
@@ -16,14 +22,24 @@ if __name__ == "__main__":
         exit(1)
     #init board 
     board = chess.Board()
-    print(board.legal_moves)
     print("Starting FEN position? (hit ENTER for standard starting postion):") 
     pos = input().strip()
-    if pos:
+    #check if pos was just enter
+    pos = pos if pos else None
+    if pos is not None:
         try:
             board.set_fen(pos)
+            print_board(board)
         except ValueError:
             print("Invalid FEN string. Using standard starting position.")
-    while game_iter(): 
+            #set default fen for side
+            board.set_fen(chess.STARTING_FEN)    
+            print_board(board)
+    else:
+        board.set_fen(chess.STARTING_FEN)  
+        print_board(board)
+    
+    #while moves sitll valid, we alternate between black and white 
+    while not board.is_check() and not board.is_stalemate(): 
         game_iter() 
     
